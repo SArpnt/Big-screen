6b9f27d806442bcc7c0fe21b6f883ac0887a0f3d
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-15 14:51:20 -0400

    seems to be done

--- Big screen.user.js
+++ Big screen.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Fullscreen
+// @name         Big screen
 // @namespace    http://tampermonkey.net/
 // @run-at       document-end
 // @version      2.0.0
-// @description  big boy
+// @description  screen is now big boy
 // @author       SArpnt
 // @match        https://boxcritters.com/play/
 // @match        https://boxcritters.com/play/?*
@@ -25,6 +25,7 @@
 			let canvas = document.getElementById('stage');
 			let worldElem = document.getElementById('world');
 			let chat = document.getElementById('chat');
+			let message = document.getElementById('message');
 			let menu = document.getElementById('menu');
 
 			let menubar = document.createElement('div');
@@ -33,61 +34,70 @@
 			menubar.className = "row justify-content-center m-0";
 			menubar.appendChild(chat.parentElement);
 			menubar.appendChild(menu.parentElement);
-			let styleTag = document.createElement('style')
+			let styleTag = document.createElement('style');
 			styleTag.innerHTML = `
-				#menubar:hover {animation-name: fadein; opacity:1}
-				#menubar {animation-name: fadeout; animation-duration: .1s; width: 100%; pointer-events: none}
+				#menubar:hover,#menubar:focus-within {opacity:1}
+				#menubar {transition: opacity .1s; opacity: .4; width: 100%; pointer-events: none}
 				#menubar > * {pointer-events: auto}
-				@keyframes fadein {0% {opacity:0.4}}
-				@keyframes fadeout {0% {opacity:1}}
-			`
-			menubar.insertAdjacentElement('afterend', styleTag)
+			`;
+			menubar.insertAdjacentElement('afterend', styleTag);
 
-			let state = 'small';
-			function setFull() {
-				state = 'full';
+			let state = ['s', 's']; // first indicates intended state, 2nd indicates actual state
+			function setFull(u = true) {
+				console.log(state, u);
+				if (state[1] == 'f') return;
+				if (u) state[0] = 'f';
+				state[1] = 'f';
 				worldElem.style.maxWidth = '100%';
+				worldElem.style.padding = '0';
 				canvas.parentElement.parentElement.style.width = 'auto';
 				canvas.parentElement.parentElement.style.maxWidth = '100%';
 				canvas.style.width = 'auto';
-				canvas.style.maxWidth = '100%';
 				menubar.style.position = 'absolute';
 				menubar.style.bottom = '0px';
-				menubar.style.opacity = ''
-				update();
+				menubar.style.opacity = '';
+				if (u) update();
 			}
-			function setBig() {
-				state = 'big';
+			function setBig(u = true) {
+				if (state[1] == 'b') return;
+				if (u) state[0] = 'b';
+				state[1] = 'b';
 				worldElem.style.maxWidth = '100%';
+				worldElem.style.padding = '0';
 				canvas.parentElement.parentElement.style.width = 'auto';
 				canvas.parentElement.parentElement.style.maxWidth = '100%';
 				canvas.style.width = 'auto';
-				canvas.style.maxWidth = '100%';
 				menubar.style.position = '';
 				menubar.style.bottom = '';
-				menubar.style.opacity = '1'
-				update();
+				menubar.style.height = '';
+				menubar.style.opacity = '1';
+				if (u) update();
 			}
-			function setSmall() {
-				state = 'small';
+			function setSmall(u = true) {
+				if (state[1] == 's') return;
+				if (u) state[0] = 's';
+				state[1] = 's';
 				worldElem.style.maxWidth = '';
+				worldElem.style.padding = '';
 				canvas.parentElement.parentElement.style.width = '';
 				canvas.parentElement.parentElement.style.maxWidth = '';
 				canvas.style.width = '100%';
-				canvas.style.maxWidth = '';
 				canvas.style.height = '';
 				menubar.style.position = '';
 				menubar.style.bottom = '';
-				menubar.style.opacity = '1'
+				menubar.style.height = '';
+				menubar.style.opacity = '1';
 			}
 
 			function update() {
-				if (state != 'small') {
-					if (state = 'full')
-						canvas.style.height = window.innerHeight + 'px';
-					else if (state = 'big')
-						canvas.style.height = (window.innerHeight - menubar.offsetHeight) + 'px';
-					canvas.style.height = Math.round(canvas.offsetWidth / world.stage.width * world.stage.height) + 'px';
+				if (state[0] != 's') {
+					if (state[0] == 'f') canvas.style.height = `${window.innerHeight}px`; // fullscreen height
+					if (state[0] == 'b' || canvas.offsetWidth > worldElem.offsetWidth) { // width fix (if window is too tall the game window would be too wide, this constrains width)
+						setBig(false);
+						canvas.style.height = `${window.innerHeight - menubar.offsetHeight}px`; // big height
+						if (canvas.offsetWidth > worldElem.offsetWidth) canvas.style.height = `${Math.round(worldElem.offsetWidth / world.stage.width * world.stage.height)}px`; // width fix
+					} else
+						setFull(false);
 				}
 			}
 			if (world.stage.hUpdate)
